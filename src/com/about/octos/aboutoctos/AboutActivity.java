47f1ec947525d56e1c2c764105412813f663f8d4
hedwig34 <hedwig34@gmail.com>
2015-06-24 13:39:43 -0400

    AboutOctOs: Add settings menu with an option to hide the app icon from launcher.
    Change-Id: I11a4eae6cf20fea008a6b9f853280f4e1562943b

--- src/com/about/octos/aboutoctos/AboutActivity.java
+++ src/com/about/octos/aboutoctos/AboutActivity.java
@@ -1,7 +1,15 @@
 package com.about.octos.aboutoctos;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class AboutActivity extends Activity {
 
@@ -9,6 +17,54 @@ public class AboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_main);
+        applyPrefs();
     }
 
+    @Override
+    public void onDestroy() {
+        applyPrefs();
+        super.onDestroy();
+
+    }
+
+    @Override
+    public void onPause(){
+        applyPrefs();
+        super.onPause();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent settingsintent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsintent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void applyPrefs(){
+
+        // Check if we should hide the launcher icon
+        PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+
+        if (settings.getBoolean("hide_icon", true)) {
+            PackageManager p=this.getPackageManager();
+            p.setComponentEnabledSetting(new ComponentName(this,AboutActivity.class), PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                    PackageManager.DONT_KILL_APP);
+        }else{
+            PackageManager p = getPackageManager();
+            ComponentName componentName = new ComponentName(this,AboutActivity.class);
+            p.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+        }
+
+    }
 }
---
+++ src/com/about/octos/aboutoctos/SettingsActivity.java
@@ -0,0 +1,25 @@
+package com.about.octos.aboutoctos;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // force it to only show the GeneralPreferenceFragment
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new GeneralPreferenceFragment()).commit();
+    }
+
+
+    public static class GeneralPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_general);
+        }
+    }
+}
